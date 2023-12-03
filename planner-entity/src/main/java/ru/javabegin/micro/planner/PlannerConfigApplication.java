b3502ae404ed9a6b485a97887127be83d282e872
Ruslan Satybaev <medtehnik850@gmail.com>
2023-12-03 21:29:04 +0700

    Lesson43: Add PlannerEntity

--- planner-entity/src/main/java/ru/javabegin/micro/planner/PlannerConfigApplication.java
+++ planner-entity/src/main/java/ru/javabegin/micro/planner/PlannerConfigApplication.java
@@ -1,11 +1,9 @@
-package ru.javabegin.micro.githubconfigserver;
+package ru.javabegin.micro.planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class PlannerConfigApplication {
 
     public static void main(String[] args) {
